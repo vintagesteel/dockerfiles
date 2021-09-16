6218971bb953dc7bc3d8fc98a338388b8c3593e8
EC2 Default User <ec2-user@ip-172-31-20-152.ec2.internal>
2021-09-16 23:20:22 +0000

    added index.js

---
+++ udemy/section4/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hi There Dude');
+});
+
+
+app.listen(8080, () => {
+	console.log('Listening on port 8080');
+});
+
+
